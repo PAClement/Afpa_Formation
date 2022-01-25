62a432bbd91334658dc6381d8fc823b17aee8da1
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-01-25 14:36:00 +0100

    add groups projects

--- ProjetGroupe/index.php
+++ ProjetGroupe/index.php
@@ -7,8 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Creative Agency</title>
 
-    <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="mobile_main.css">
+    <!-- CLEMENT CSS  -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mobile_main.css">
+
+    <!-- ALEXANDRE CSS -->
+    <link rel="stylesheet" href="css/alex_style.css">
+
+    <!-- QUENTIN CSS -->
+    <link rel="stylesheet" href="css/quentin_style.css">
+
+    <!-- EVAN CSS -->
+    <link rel="stylesheet" href="css/evan_style.css">
+    <link rel="stylesheet" href="css/evan2_style.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
