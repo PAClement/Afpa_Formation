e92b17964a480129c87ba3daf20c39dcf4777111
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-02-07 08:40:36 +0100

    learn bootstrap

--- Tp_bootstrap/index.php
+++ Tp_bootstrap/index.php
@@ -16,10 +16,33 @@
 
     <?php include('includes/_header.php'); ?>
 
+    <button type="button" class="btn btn-primary mt-5" id="liveToastBtn">Show live toast</button>
 
+    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+        <div id="liveToast" class="toast" style="position: absolute; top: 0; right: 0;">
+            <div class="toast-header">
+                <img src="..." class="rounded mr-2" alt="...">
+                <strong class="mr-auto">Bootstrap</strong>
+                <small>11 mins ago</small>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+            </div>
+        </div>
+    </div>
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $("#liveToastBtn").click(function() {
+
+            $("#liveToast").toast("show");
+
+        });
+    </script>
 </body>
 
 </html>
