589f67733dcb4e18e42414802a95dce543094d4c
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-03-07 08:37:37 +0100

    new commit

--- jquery/Tp_JQ_1/assets/js/main.js
+++ jquery/Tp_JQ_1/assets/js/main.js
@@ -3,8 +3,6 @@ let tab = [];
 $('form').submit(function (e) {
     e.preventDefault();
 
-    $('h5').addClass('d-none');
-
     $('input').each(function () {
         tab.push($(this).val());
     });
@@ -12,12 +10,13 @@ $('form').submit(function (e) {
     let current = { produit: tab[0], marque: tab[1], model: tab[2], ref: tab[3], spec: tab[4] };
     tab = [];
 
-    $('.copy').removeClass("d-none");
-
     $('textarea').val(`${current.produit} ${current.marque}\n${current.marque} ${current.model}\n${current.marque} ${current.ref}\n${current.produit} ${current.marque} ${current.spec}\n${current.marque} ${current.spec}`);
+
+    $('h5').addClass('d-none');
+    $('.copy').removeClass("d-none");
 });
 
-$('.copy').click(function () {
+$('.clipboard').click(function () {
 
     navigator.clipboard.writeText($('textarea').val());
     $('h5').removeClass('d-none');
