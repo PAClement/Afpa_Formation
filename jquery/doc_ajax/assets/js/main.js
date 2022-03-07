92ba14ad91a5b0ab2a9f6cd445404420147f10dc
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-03-07 13:19:21 +0100

    New Commit

---
+++ jquery/doc_ajax/assets/js/main.js
@@ -0,0 +1,37 @@
+
+let request = $.ajax({
+    type: "GET",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    dataType: "json",
+});
+
+request.done(function (response) {
+
+    let display = '';
+    for (let i = 0; i < response.length; i++) {
+        display += `
+        <div class="card m-1" style="width: 18rem;">
+            <div class="card-header bg-white border-0">
+            <button type="button" class="btn btn-primary">Numéro de la tâche : <span class="badge bg-white text-dark">${response[i].id}</span></button>
+            </div>
+            <div class="card-body">
+                <p class="card-text">${response[i].title}</p>
+            </div>
+            <div class="card-footer bg-white border-0">
+            ${response[i].completed ? `<button type="button" class="btn btn-success">Terminée</button>` : `<button type="button" class="btn btn-warning">En cours</button>`}
+            </div>
+        </div>
+        `;
+    }
+
+    $('#root').html(display);
+});
+
+request.fail(function (http_error) {
+    //Code à jouer en cas d'éxécution en erreur du script du PHP
+
+    let server_msg = http_error.responseText;
+    let code = http_error.status;
+    let code_label = http_error.statusText;
+    alert("Erreur " + code + " (" + code_label + ") : " + server_msg);
+});
---
+++ jquery/doc_ajax/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JQuery Ajax</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+
+<body>
+    <nav class="py-3 bg-light">
+        <p class=" mb-0 fs-1 text-center">Liste des tâches</p>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col mt-3">
+                <div id="root" class="row d-flex justify-content-center">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="assets/js/jquery-3.6.0.min.js">
+    </script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
