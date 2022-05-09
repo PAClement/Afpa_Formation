df8a56c055a35ca2dbb0bcca9341cada3b9c8cf1
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-05-09 16:58:36 +0200

    add search + pagination

--- SYMFONY/blog/src/Controller/HomeController.php
+++ SYMFONY/blog/src/Controller/HomeController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AbstractController
 {
@@ -78,6 +79,36 @@ class HomeController extends AbstractController
 
             return $this->redirectToRoute("app_home");
         }
+        return $this->render('home/index.html.twig', [
+            "data" => $articlesPag,
+            "categories" => $this->cr->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/recherche", name="recherche")
+     */
+    public function recherche(Request $request, PaginatorInterface $paginator): Response
+    {
+        $date = \DateTime::createFromFormat("Y-m-d", date($request->request->get('date')));
+        //dd($date);
+
+        $session = new Session();
+        if (!$session->get('articles')) {
+            $session->start();
+        }
+
+        if ($request->request->get('title')) {
+            $articles = $this->ar->findByTitleLike($request->request->get('title'), $date);
+            $session->set('articles', $articles);
+        }
+
+        $articlesPag = $paginator->paginate(
+            $session->get('articles'), // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+
         return $this->render('home/index.html.twig', [
             "data" => $articlesPag,
             "categories" => $this->cr->findAll()
